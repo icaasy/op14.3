6d344f2c90ab32038afb96acde4cd30f3ca0432d
Anton <anton.m.lotos@gmail.com>
2024-02-18 18:26:57 +0300

    append push / pop functions

--- main.c
+++ main.c
@@ -729,6 +729,152 @@ void test_deleteVectorV() {
     assert(vec.size == 0);
     assert(vec.data == NULL);
 }
+//
+bool test_isEmptyV() {
+    vectorVoid vec = createVectorV(6, sizeof(int));
+
+    bool res = isEmptyV(&vec);
+
+    assert(res);
+}
+
+bool test_isFullV() {
+    vectorVoid vec = createVectorV(6, sizeof(int));
+
+    bool res = isFullV(&vec);
+
+    assert(!res);
+}
+
+void test_getVectorValueV() {
+    vectorVoid vec = createVectorV(6, sizeof(int));
+
+    int x = 4;
+
+    int i = 1;
+    pushBackV(&vec, &i);
+    i = 2;
+    pushBackV(&vec, &i);
+    i = 3;
+    pushBackV(&vec, &x);
+
+    int j = 3;
+    getVectorValueV(&vec, 2, &j);
+
+    assert(&j != &i);
+}
+
+void test_setVectorValueV() {
+    vectorVoid vec = createVectorV(6, sizeof(int));
+
+    int x = 4;
+
+    int i = 1;
+    pushBackV(&vec, &i);
+    i = 2;
+    pushBackV(&vec, &i);
+    i = 3;
+    setVectorValueV(&vec, 1, &x);
+
+    int j = 3;
+    getVectorValueV(&vec, 1, &j);
+
+    assert(&j != &i);
+}
+
+void test_popBackV() {
+    vectorVoid vec = createVectorV(0, sizeof(int));
+
+    int j = 10;
+    pushBackV(&vec, &j);
+
+    assert(vec.size == 1);
+
+    popBackV(&vec);
+
+    assert(vec.size == 0);
+    assert(vec.capacity == 1);
+}
+
+void test_pushBack_emptyVectorV() {
+    vectorVoid vec = createVectorV(0, sizeof(int));
+
+    assert(vec.capacity == 0);
+    assert(vec.size == 0);
+    assert(vec.data != NULL);
+
+    int x = 4;
+    pushBackV(&vec, &x);
+
+    assert(vec.capacity == 1);
+    assert(vec.size == 1);
+    assert(vec.data != NULL);
+
+    assert(((int *) vec.data)[0] == 4);
+}
+
+void test_pushBack_fullVectorV() {
+    vectorVoid vec = createVectorV(2, sizeof(int));
+
+    assert(vec.capacity == 2);
+    assert(vec.size == 0);
+    assert(vec.data != NULL);
+
+    int a = 1;
+    int x = 4;
+    pushBackV(&vec, &a);
+    pushBackV(&vec, &x);
+
+    assert(vec.capacity == 2);
+    assert(vec.size == 2);
+    assert(vec.data != NULL);
+
+    assert(((int *) vec.data)[1] == 4);
+
+    int y = 5;
+    pushBackV(&vec, &y);
+
+    assert(vec.capacity == 4);
+    assert(vec.size == 3);
+    assert(vec.data != NULL);
+
+    assert(((int *) vec.data)[0] == 1);
+    assert(((int *) vec.data)[2] == 5);
+}
+
+void test_functional1() {
+    int n = 10;
+
+    int test_data[] = {3, 6, 7, 1, 0, 8, 9, 3, 2, 16};
+
+    vectorVoid v = createVectorV(0, sizeof(int));
+    for (int i = 0; i < n; i++) {
+        int x = test_data[i];
+        pushBackV(&v, &x);
+    }
+    for (int i = 0; i < n; i++) {
+        int x;
+        getVectorValueV(&v, i, &x);
+        assert(x == test_data[i]);
+    }
+}
+
+void test_functional2() {
+    int n = 10;
+
+    float test_data[] = {3.12f, 6.124f, 7.14f, 1.891f, 0.13f, 8.1245f, 9.03f, 3.463f, 2.124f, 16.096f};
+
+    vectorVoid v = createVectorV(0, sizeof(float));
+    for (int i = 0; i < n; i++) {
+        float x = test_data[i];
+        pushBackV(&v, &x);
+    }
+    for (int i = 0; i < n; i++) {
+        float x;
+        getVectorValueV(&v, i, &x);
+        assert(x == test_data[i]);
+    }
+}
 
 void test() {
     test_createVector();
@@ -754,13 +900,22 @@ void test() {
     test_shrinkToFitV();
     test_clearV();
     test_deleteVectorV();
-
+    test_isEmptyV();
+    test_isFullV();
+    test_getVectorValueV();
+    test_setVectorValueV();
+    test_popBackV();
+    test_pushBack_emptyVectorV();
+    test_pushBack_fullVectorV();
+    test_functional1();
+    test_functional2();
 }
 
 int main() {
     test();
-
     vectorVoid test;
+
+
 
     return 0;
 }
